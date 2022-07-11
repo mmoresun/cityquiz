8a3ef559209a0161724f84b4e7ffbf4a732e9730
Anton <zzzebra@gmail.com>
2022-07-11 13:43:47 +0300

    Updates

---
+++ precache-manifest.0043b688b5fdeeaab9ccdab1c3693a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cf3125bde81c5c4fac7",
+    "url": "/cityquiz/static/css/main.bb479367.chunk.css"
+  },
+  {
+    "revision": "4cf3125bde81c5c4fac7",
+    "url": "/cityquiz/static/js/main.262595ad.chunk.js"
+  },
+  {
+    "revision": "13b121117e1254223151",
+    "url": "/cityquiz/static/js/runtime~main.a9720462.js"
+  },
+  {
+    "revision": "cd1773aebc89f9e22ef6",
+    "url": "/cityquiz/static/js/2.7ecd9b0d.chunk.js"
+  },
+  {
+    "revision": "0c8ea1b911b8c4b9b4b71e0947b0f77a",
+    "url": "/cityquiz/index.html"
+  }
+];
